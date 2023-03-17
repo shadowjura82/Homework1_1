307d66b874f40f6c6d8ead0be21b219bc2673b3f
Yurii <shadowjura82@gmail.com>
2023-03-17 07:33:27 +0100

    Converting a row to a column

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        // new branch has been created
+        System.out.println("Please enter any message for SkyPro");
+        Scanner inp = new Scanner(System.in);
+        String str = inp.nextLine();
+        String[] strArray = str.split(" ");
+        for (String x : strArray) {
+            System.out.println(x);
+        }
     }
 }
